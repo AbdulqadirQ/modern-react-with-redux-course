4e3ded081e467eaa99d57dc6bd8f415eb9701988
abdul.qasim <abdul.qasim@baesystems.com>
2019-05-29 16:19:43 +0100

    lec 83: preventDefault used on form to disable page refresh on each submit

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,10 +3,14 @@ import React from 'react';
 class SearchBar extends React.Component {
     state = { term: '' };
 
+    onFormSubmit(event) {
+        event.preventDefault(); // prevents form being submitted (and consequently page refreshing)
+    }
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
                         <input
