aafd0cf5e293fd4455da82c44a572a79ab8b6058
abdul.qasim <abdul.qasim@baesystems.com>
2019-05-31 16:42:48 +0100

    lec 111: implement SearchBar component with text and submit handlers

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -1,8 +1,29 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+    state = { term : '' };
+
+    onChangeText = (event) => {
+        this.setState({term : event.target.value})
+    }
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        // TODO: Ensure to call callback from parent component
+    }
+
     render(){
-        return <div>SearchBar</div>;
+        return (
+            <div className="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input value={this.state.term} onChange={this.onChangeText} type="text" />
+                    </div>
+                </form>
+            </div>
+        );
     }
 }
 
